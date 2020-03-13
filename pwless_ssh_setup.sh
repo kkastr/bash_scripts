9e7bcff7b3e443cfb4164585cd004844f9e53d03
kkastr <dinokastritis@gmail.com>
2020-03-13 16:38:31 -0400

    added conditional requirement for port number and made the script check if the alias is already in use

--- pwless_ssh_setup.sh
+++ pwless_ssh_setup.sh
@@ -7,12 +7,26 @@
 #$4 is port used for connection ( each 22xx corresponds to a different machine in the MCS network )
 
 
-echo '\n' >> ~/.ssh/config
+exists=$(grep -c $3 ~/.ssh/config)
+
+# apparently better than numeric comparison
+if [ $exists -gt "0" ]; then
+    echo "Host with provided alias already exists."
+    exit
+fi
+
+re='^[0-9]+$' #regex magic
+
+
 echo "Host $3" >> ~/.ssh/config
 echo "    HostName $2" >> ~/.ssh/config
-echo "    Port $4" >> ~/.ssh/config
+if [[ $4 =~ $re ]]; then
+    echo "    Port $4" >> ~/.ssh/config
+fi
 echo "    User $1" >> ~/.ssh/config
 
+
+#make this in case it doesn't exist
 ssh $3 mkdir -p .ssh
 
 cat ~/.ssh/id_rsa.pub | ssh $3 'cat >> .ssh/authorized_keys'
